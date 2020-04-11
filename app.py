17072ac65c611c8f9612bea4148ec6d21669879c
Ayl0002 <ayl0002@gmail.com>
2020-04-11 13:47:23 -0500

    Third Commit

--- app.py
+++ app.py
@@ -1,31 +1,18 @@
 from flask import Flask, jsonify, render_template
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
 import pandas as pd
 import psycopg2
 
-# rds_connection_string = "postgres:postgres@localhost:5432/netflix_db"
-# engine = create_engine(f'postgresql://{rds_connection_string}')
-#
-# pd.read_sql_query('select * from Netflix1', con=engine)
-#
-#
-# # Save reference to the table
-# netflix = Base.classes.Netflix1
-
 app = Flask(__name__)
 
-# @app.route("/")
-# def welcome():
+@app.route("/")
+def welcome():
 
-#     return render_template("index.html")
+    return render_template("index.html")
 
 # @app.route("/maps")
 # def maps():
 
-#     return render_template("")
+#     return render_template("")           
 
 # @app.route("/comparison-by-movies-shows")
 # def compareMovies():
@@ -41,12 +28,9 @@ app = Flask(__name__)
 
 @app.route("/data")
 def data():
-    con = psycopg2.connect("host='localhost' dbname='Netflix-1' user='postgres' password='R@dium.33'")
-    cur = con.cursor()
-    cur.execute('''select * from public."Netflix1"''')
-    data = [col for col in cur]
-    cur.close()
+    con = psycopg2.connect("host='localhost' dbname='Netflix' user='postgres' password='R@dium.33'")
+    data = pd.read_sql('''select * from  public."Netflix1"''', con=con)
+    data = data.to_dict(orient="records")
     return jsonify(data)
-
 if __name__ == "__main__":
     app.run(debug=True)
