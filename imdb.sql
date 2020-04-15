87112a72dc945ced5e348cad8667cec334ea99f7
ayl0002 <57423347+ayl0002@users.noreply.github.com>
2020-04-14 21:20:26 -0500

    Add files via upload

--- imdb.sql
+++ imdb.sql
@@ -1,8 +1,4 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Link to schema: https://app.quickdatabasediagrams.com/#/d/tljoTc
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-
-
+--Create Master Table--
 CREATE TABLE "IMDB5000" (
     "serial" serial   NOT NULL,
     "color" VARCHAR   NULL,
@@ -33,8 +29,48 @@ CREATE TABLE "IMDB5000" (
     "imdb_score" FLOAT   NULL,
     "aspect_ratio" FLOAT   NULL,
     "movie_facebook_likes" INT   NULL,
-    CONSTRAINT "pk_Netflix1" PRIMARY KEY (
+    CONSTRAINT "pk_IMDB5000" PRIMARY KEY (
         "serial"
      )
 );
+
+
+--Poplate Master Table by Importing IMDB CSV--
+
+--Create Map Table--
+CREATE TABLE "Movie_Map" (
+    "Serial" Serial   NOT NULL,
+    "movie_title" VARCHAR   NULL,
+    "gross" INTEGER   NULL,
+    "director_name" VARCHAR   NULL,
+    "actor_1_name" VARCHAR   NULL,
+    "actor_2_name" VARCHAR   NULL,
+    "actor_3_name" VARCHAR   NULL,
+    "country" VARCHAR   NULL,
+    "imdb_score" FLOAT(2)   NULL,
+    CONSTRAINT "pk_Movie_Map" PRIMARY KEY (
+        "Serial"
+     )
+);
+
+--Create Budget Table--
+CREATE TABLE "Budget_Table" (
+    "Serial" Serial   NOT NULL,
+    "movie_title" VARCHAR   NULL,
+    "gross" FLOAT(2)   NULL,
+   	"budget" FLOAT(2)   NULL,
+    "imdb_score" FLOAT(2)   NULL,
+    CONSTRAINT "pk_Budget_Table" PRIMARY KEY (
+        "Serial"
+     )
+);
+
+--Populate Budget Table from Master--
+INSERT INTO public."Budget_Table" ("movie_title","gross","budget","imdb_score")
+SELECT "movie_title","gross","budget","imdb_score" FROM public."IMDB5000";
+
+--Populate Map Table from Master--
+INSERT INTO public."Movie_Map" ("movie_title","gross","director_name","actor_1_name","actor_2_name","actor_3_name","country","imdb_score")
+SELECT "movie_title","gross","director_name","actor_1_name","actor_2_name","actor_3_name","country",CAST("imdb_score" AS FLOAT(2)) AS "imdb_score"
+FROM public."IMDB5000";
 
