7ff072afeb2db20a71159609224c4d805cc37f98
gregobrien1613 <55119423+gregobrien1613@users.noreply.github.com>
2020-04-16 20:43:19 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
 from flask import Flask, jsonify, render_template
 import psycopg2
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
 import pandas as pd
-
+import json
 
 app = Flask(__name__)
 
@@ -14,39 +10,26 @@ def welcome():
 
     return render_template("index.html")
 
-
-# @app.route("/maps")
-# def maps():
-#
-#     return render_template("")
-#
-# @app.route("/comparison-by-movies-shows")
-# def compareMovies():
-#
-#     return render_template("")
-#
-# @app.route("/comparison-by-genre")
-# def compareGenres():
-#
-#     return render_template("")
-#
-
-
-
-
 @app.route("/data")
 def data():
-    con = psycopg2.connect("host='localhost' dbname='Netlix-1' user='postgres' password='postgres'")
-    data = pd.read_sql('''select * from  public."Netflix1"''', con=con)
-    data = data.to_dict(orient="records")
-    return jsonify(data)
+    con = psycopg2.connect("host='localhost' dbname='Netflix' user='postgres' password='postgres'")
+    data = pd.read_sql('''select * from  public."IMDB5000"''', con=con)
+    data = data.to_json(orient='records',date_format='iso')
+    return jsonify(json.loads(data))
 
 @app.route("/data2")
 def data2():
-    con = psycopg2.connect("host='localhost' dbname='Netlix-1' user='postgres' password='postgres'")
+    con = psycopg2.connect("host='localhost' dbname='Netflix' user='postgres' password='postgres'")
     data2 = pd.read_sql('''select * from  public."Budget_Table"''', con=con)
-    data2 = data2.to_dict(orient="records")
-    return jsonify(data2)
+    data2 = data2.to_json(orient='records',date_format='iso')
+    return jsonify(json.loads(data2))
+
+@app.route("/data3")
+def data3():
+    con = psycopg2.connect("host='localhost' dbname='Netflix' user='postgres' password='postgres'")
+    data3 = pd.read_sql('''SELECT * FROM public."movie_map_final"''', con=con)
+    data3 = data3.to_json(orient='records',date_format='iso')
+    return jsonify(json.loads(data3))
 
 if __name__ == "__main__":
     app.run(debug=True)
