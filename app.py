25306208bef0305ec67bb59f137cf754457c3a8c
gregobrien1613 <55119423+gregobrien1613@users.noreply.github.com>
2020-04-16 20:32:09 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, render_template
-import pandas as pd
 import psycopg2
+import pandas as pd
+import json
 
 app = Flask(__name__)
 
@@ -9,41 +10,26 @@ def welcome():
 
     return render_template("index.html")
 
-# @app.route("/maps")
-# def maps():
-
-#     return render_template("")           
-
-# @app.route("/comparison-by-movies-shows")
-# def compareMovies():
-
-#     return render_template("")
-
-# @app.route("/comparison-by-genre")
-# def compareGenres():
-
-#     return render_template("")
-  
-@app.route("/data")             
+@app.route("/data")
 def data():
-    con = psycopg2.connect("host='localhost' dbname='Netflix' user='postgres' password='R@dium.33'")
+    con = psycopg2.connect("host='localhost' dbname='Netflix' user='postgres' password='postgres'")
     data = pd.read_sql('''select * from  public."IMDB5000"''', con=con)
-    data = data.to_dict(orient="records")
-    return jsonify(data)
+    data = data.to_json(orient='records',date_format='iso')
+    return jsonify(json.loads(data))
 
 @app.route("/data2")
 def data2():
-    con = psycopg2.connect("host='localhost' dbname='Netflix' user='postgres' password='R@dium.33'")
-    data2 = pd.read_sql('''SELECT * FROM public."Movie_Map"''', con=con)
-    data2 = data2.to_dict(orient="records")
-    return jsonify(data2)
+    con = psycopg2.connect("host='localhost' dbname='Netlix-1' user='postgres' password='postgres'")
+    data2 = pd.read_sql('''select * from  public."Budget_Table"''', con=con)
+    data2 = data2.to_json(orient='records',date_format='iso')
+    return jsonify(json.loads(data2))
 
 @app.route("/data3")
 def data3():
-    con = psycopg2.connect("host='localhost' dbname='Netflix' user='postgres' password='R@dium.33'")
-    data3 = pd.read_sql('''SELECT * FROM public."Budget_Table"''', con=con)
-    data3 = data3.to_dict(orient="records")
-    return jsonify(data3)
+    con = psycopg2.connect("host='localhost' dbname='Netflix' user='postgres' password='postgres'")
+    data3 = pd.read_sql('''SELECT * FROM public."movie_map_final"''', con=con)
+    data3 = data3.to_json(orient='records',date_format='iso')
+    return jsonify(json.loads(data3))
 
 if __name__ == "__main__":
     app.run(debug=True)
