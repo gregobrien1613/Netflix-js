d628174527d05e669b817afd764bd9201afec549
gregobrien1613 <55119423+gregobrien1613@users.noreply.github.com>
2020-04-16 19:23:52 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,18 +1,11 @@
 from flask import Flask, jsonify, render_template
+import psycopg2
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 import pandas as pd
 
-# rds_connection_string = "postgres:postgres@localhost:5432/netflix_db"
-# engine = create_engine(f'postgresql://{rds_connection_string}')
-#
-# pd.read_sql_query('select * from Netflix1', con=engine)
-#
-#
-# # Save reference to the table
-# netflix = Base.classes.Netflix1
 
 app = Flask(__name__)
 
@@ -21,31 +14,39 @@ def welcome():
 
     return render_template("index.html")
 
-@app.route("/maps")
-def maps():
 
-    return render_template("")
+# @app.route("/maps")
+# def maps():
+#
+#     return render_template("")
+#
+# @app.route("/comparison-by-movies-shows")
+# def compareMovies():
+#
+#     return render_template("")
+#
+# @app.route("/comparison-by-genre")
+# def compareGenres():
+#
+#     return render_template("")
+#
 
-@app.route("/comparison-by-movies-shows")
-def compareMovies():
 
-    return render_template("")
 
-@app.route("/comparison-by-genre")
-def compareGenres():
-
-    return render_template("")
-
-    
 
 @app.route("/data")
 def data():
-    con = psycopg2.connect("host='localhost' dbname='Netflix1' user='postgres' password='postgres'")
-    cur = con.cursor()
-    cur.execute("""select * from  Netflix1""")
-    data = [col for col in cur]
-    cur.close()
+    con = psycopg2.connect("host='localhost' dbname='Netlix-1' user='postgres' password='postgres'")
+    data = pd.read_sql('''select * from  public."Netflix1"''', con=con)
+    data = data.to_dict(orient="records")
     return jsonify(data)
+
+@app.route("/data2")
+def data2():
+    con = psycopg2.connect("host='localhost' dbname='Netlix-1' user='postgres' password='postgres'")
+    data2 = pd.read_sql('''select * from  public."Budget_Table"''', con=con)
+    data2 = data2.to_dict(orient="records")
+    return jsonify(data2)
 
 if __name__ == "__main__":
     app.run(debug=True)
