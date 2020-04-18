dd6da1d834841ce42c569c15203ea725a21ff933
ayl0002 <57423347+ayl0002@users.noreply.github.com>
2020-04-17 20:24:17 -0500

    Add files via upload

---
+++ templates/map_static/js/js_example.js
@@ -0,0 +1,122 @@
+//Function expression to select elements
+const selectElement = (s) => document.querySelector(s);
+
+//Open the menu on click
+selectElement('.open') .addEventListener('click', () => {
+  selectElement('.nav-list').classList.add('active');
+});
+
+//Close the menu on click
+selectElement('.close') .addEventListener('click', () => {
+  selectElement('.nav-list').classList.remove('active');
+});
+
+var query3 = "/data3"
+
+d3.json(query3).then(function(data) {
+  createMap(data);
+});
+
+function bindPopMaker(feature, layer) {
+  layer.bindPopup("<h3>" + feature.country +
+    "</h3><hr><p>" + feature.total_movies + "</p>");
+}
+
+function ColorCheck(d) {
+  if (d >= 100000000000) {
+      return "red";
+  } else if (d >= 10000000000) {
+      return "orange";
+  } else if (d >= 5000000000) {
+      return "yellow";
+  } else if (d >= 10000000) {
+      return "green";
+  } else if (d >= 1000000) {
+      return "blue";
+  }
+   else {
+      return "black";
+  };
+}
+
+function radiusScale(d) {
+  if (d >= 100000000000) {
+      return 500000;
+  } else if (d >= 10000000000) {
+      return 200000;
+  }    
+    else {
+      return 150000;
+  };
+}
+
+function createMap(movieData) {
+movie_Data = movieData.map((feature) =>
+L.circle([feature.CapitalLatitude,feature.CapitalLongitude], {
+  color: ColorCheck(feature.total_budget),
+  fillColor: ColorCheck(feature.total_budget),
+  fillOpacity: 0.75,
+  radius:  radiusScale(feature.total_budget)
+}).bindPopup("<h2> Country : " + feature.country +
+"</h2><hr><h3> Total Movies: " + feature.total_movies +
+"</h3><hr><p> Total Budget: " + feature.budget_thousands + 
+"</h4><hr><p> Avg IMDB: " + feature.avg_imdb +
+"</p>")
+)
+
+var movie_Data=L.layerGroup(movie_Data)
+  
+   var worldmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.streets",
+    accessToken: API_KEY
+  });
+
+  var baseMaps = {
+    "World Map" : worldmap
+  
+  };
+
+  
+  var overlayMaps = {
+    Movie_Data: movie_Data
+  };
+
+  
+  var myMap = L.map("map", {
+    center: [
+      0,15
+    ],
+    zoom: 2.5,
+    layers: [worldmap,movie_Data]
+  });
+
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
+
+
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function () {
+
+  var div = L.DomUtil.create('div', 'info legend'),
+  labels = ['<strong>Categories</strong>'],
+  categories = [999999, 1000000, 10000000, 5000000000, 10000000000, 100000000000];
+
+  for (var i = 0; i < categories.length; i++) {
+    div.innerHTML += 
+    labels.push(
+        '<i style="background:' + ColorCheck(categories[i]) + '"></i> ' +
+        (categories[i] ? categories[i] : '+'));
+}
+
+div.innerHTML = labels.join('<br>');
+return div;
+
+ };
+
+legend.addTo(myMap);
+  
+}
